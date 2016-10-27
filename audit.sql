576a862ff232959705af57fe547d2bdfc5a7dfea
Hugo Mercier <hugo.mercier@oslandia.com>
2016-10-27 10:33:04 +0200

    Add a function to replay a logged event

--- audit.sql
+++ audit.sql
@@ -239,3 +239,50 @@ $body$ LANGUAGE 'sql';
 COMMENT ON FUNCTION audit.audit_table(regclass) IS $body$
 Add auditing support to the given table. Row-level changes will be logged with full client query text. No cols are ignored.
 $body$;
+
+CREATE OR REPLACE FUNCTION audit.replay_event(pevent_id int) RETURNS void AS $body$
+DECLARE
+  query text;
+BEGIN
+    with
+    event as (
+        select * from audit.logged_actions where event_id = pevent_id
+    )
+    -- get primary key names
+    , where_pks as (
+        select array_to_string(array_agg(a.attname || '=' || quote_literal(row_data->a.attname)), ' AND ') as where_clause
+          from pg_index i
+          join pg_attribute a on a.attrelid = i.indrelid
+                             and a.attnum = any(i.indkey)
+          join event on i.indrelid = (schema_name || '.' || table_name)::regclass
+          where         i.indisprimary
+    )
+    select into query
+        case
+            when action = 'I' then
+                'INSERT INTO ' || schema_name || '.' || table_name ||
+                ' ('||(select string_agg(key, ',') from each(row_data))||') VALUES ' ||
+                '('||(select string_agg(case when value is null then 'null' else quote_literal(value) end, ',') from each(row_data))||')'
+            when action = 'D' then
+                'DELETE FROM ' || schema_name || '.' || table_name ||
+                ' WHERE ' || where_clause
+            when action = 'U' then
+                'UPDATE ' || schema_name || '.' || table_name ||
+                ' SET ' || (select string_agg(key || '=' || case when value is null then 'null' else quote_literal(value) end, ',') from each(changed_fields)) ||
+                ' WHERE ' || where_clause
+        end
+    from
+        event, where_pks
+    ;
+    
+    execute query;
+END;
+$body$
+LANGUAGE plpgsql;
+
+COMMENT ON FUNCTION audit.replay_event(int) IS $body$
+Replay a logged event.
+ 
+Arguments:
+   pevent_id:  The event_id of the event in logged_actions to replay
+$body$;
